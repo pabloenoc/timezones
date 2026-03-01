bc6efa5ef22de77798511d14e85211c553d0a80a
amalia-ullman <mali.ullman@gmail.com>
2026-03-01 12:14:24 -0800

    random london map

--- app.js
+++ app.js
@@ -12,4 +12,10 @@ async function get_timezone_data() {
     console.log(location_data);
 }
 
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
 get_timezone_data();
