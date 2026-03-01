a895000cd775578f7d8e1725045333608d06ff95
amalia-ullman <mali.ullman@gmail.com>
2026-03-01 10:54:06 -0800

    WHAT!!

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ base_url = "/timezones";
 
 async function getTimezoneData() {
     const requestURL =
-        `/data.json`;
+        `/timezones/data.json`;
     const request = new Request(requestURL);
 
     const response = await fetch(request);
