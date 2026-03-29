66e834da4ecb3110094a2a207b84801cee78e9e4
amalia-ullman <mali.ullman@gmail.com>
2026-03-29 11:08:10 -0700

    pan only u.s.

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ async function get_timezone_data() {
     console.log(location_data);
 }
 
-var map = L.map('map').setView([39.833333, -98.583333], 4);
+var map = L.map('map').setView([39.833333, -98.583333], 4)
+map.setMaxBounds(L.latLngBounds(L.latLng(23, -125.42), L.latLng(49, -62.03)));
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
